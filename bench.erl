a66b06eaec92b36861a70e09b6ba000f674d58cf
Max Lapshin <max@maxidoors.ru>
2012-10-20 12:15:37 +0400

    bench now is accepting path and print properly timings

--- bench.erl
+++ bench.erl
@@ -2,8 +2,8 @@
 
 -mode(compile).
 
-main([]) ->
-  {ok,Bin} = file:read_file("m.xml"),
+main([Path]) ->
+  {ok,Bin} = file:read_file(Path),
   List = binary_to_list(Bin),
   code:add_pathz("ebin"),
   Count = 100,
@@ -14,19 +14,19 @@ main([]) ->
   spawn(fun() -> loop_xmerl(List, Count), 
     Self ! {ready, xmerl, timer:now_diff(erlang:now(),T0),process_info(self(),memory)} end),
   receive
-    {ready,xmerl,T1,{memory,M1}} -> io:format("   xmerl: ~8.. B ~8.. B~n", [T1,M1])
+    {ready,xmerl,T1,{memory,M1}} -> io:format("   xmerl: ~8.. Bms ~8.. BKB~n", [T1 div 1000,M1 div 1024])
   end,
   receive
-    {ready,parsexml,T2,{memory,M2}} -> io:format("parsexml: ~8.. B ~8.. B~n", [T2,M2])
+    {ready,parsexml,T2,{memory,M2}} -> io:format("parsexml: ~8.. Bms ~8.. BKB~n", [T2 div 1000,M2 div 1024])
   end,
   ok.
 
 loop_parsexml(_Bin,0) -> ok;
 loop_parsexml(Bin,Count) ->
-  A = parsexml:parse(Bin),
+  _A = parsexml:parse(Bin),
   loop_parsexml(Bin,Count-1).
 
 loop_xmerl(_Bin,0) -> ok;
 loop_xmerl(Bin,Count) ->
-  A = xmerl_scan:string(Bin),
+  _A = xmerl_scan:string(Bin),
   loop_xmerl(Bin,Count-1).
